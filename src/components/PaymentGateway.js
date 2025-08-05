a58df867c7d96754770820990493bfd13dd6fde2
Christopher Guaña <132909268+Christopher9653@users.noreply.github.com>
2025-08-05 14:07:04 -0500

    Update PaymentGateway.js

--- src/components/PaymentGateway.js
+++ src/components/PaymentGateway.js
@@ -127,7 +127,7 @@ const PaymentGateway = ({ total, onPaymentSuccess, onPaymentError, onBackToCheck
                 placeholder="XXX"
                 className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500/50"
                 required
-                maxLength="4"
+                maxLength="3"
                 inputMode="numeric"
               />
             </div>
